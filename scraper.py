16ddca8f667c69c0da296345eb5b08ea921a848f
ElijahZAwesome <elijahbansley@monroe.k12.wi.us>
2019-07-19 22:54:53 -0500

    Display segment coded, needs linux testing

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ except ImportError:
     import urllib as urlrequest
 from bs4 import BeautifulSoup
 import sys
+import requests
+import os
 
 # thegamesdb.net base URLs (Searching, platform listing, anything else needed)
 base_search = "https://thegamesdb.net/search.php?platform_id%5B%5D=0&name="
@@ -14,8 +16,26 @@ def get_html_search(searchterm):
     search_page = urlrequest.urlopen(base_search + searchterm)
     html = BeautifulSoup(search_page, "html.parser")
     print(html)
+    
+def save_art(url, filename):
+    #wget.download(url, filename)
+    
+    # TheGamesDB blocks urlopen, so we need to use an alternative method
+    
+    # Get the art via the URL
+    r = requests.get(url) # create HTTP response object 
+  
+    # Open the image file and write the request data to it.
+    with open(filename,'wb') as f: 
+        f.write(r.content) 
+        f.close()
+
+def open_art(filename):
+    os.startfile(filename)
 
 if len(sys.argv) < 2:
     print("Usage: scraper.py <search term>")
     sys.exit()
 get_html_search(sys.argv[1])
+save_art(sys.argv[2], "test.png")
+open_art("test.png")
